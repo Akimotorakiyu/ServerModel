d62199c054e2702e65ecfe5939a06cbf3a642f20
鸽子·湫曗 <autumnsilverleaves@gmail.com>
2019-12-01 21:32:07 +0800

    简化请求测试

--- example/index.ts
+++ example/index.ts
@@ -1,38 +1,19 @@
-import tsKoa from "../lib/index"
+import tsKoa from "../lib/index";
 
 const app = new tsKoa();
 
 app.use(async (ctx, next) => {
-    console.log("get into 1");
-    ctx.res.write("hello world! 1");
+  console.log("get into 1");
+  ctx.res.write("hello world! 1");
+  console.log(ctx.req.headers);
 
-    next();
-})
+  ctx.req.on("data", data => {
+    console.log(data);
+  });
 
-app.use(async (ctx, next) => {
-    console.log("get into 2")
-    ctx.res.write("hello world! 2");
-    next();
-})
+  next();
+});
 
 app.createServer().listen("666", () => {
-    console.log("tsKoa serveing...");
-})
-
-app.createServer().listen("777", () => {
-    console.log("tsKoa serveing...");
-})
-
-app.use(async (ctx, next) => {
-    console.log("get into 3");
-    ctx.res.write("hello world! 3");
-    next();
-})
-
-setTimeout(() => {
-    app.use(async (ctx, next) => {
-        console.log("get into 4");
-        ctx.res.write("hello world! 4");
-        next();
-    })
-}, 3000)
+  console.log("tsKoa serveing...");
+});
