29a82331f73b7587aa376ebf439da788fef42704
鸽子·湫曗 <autumnsilverleaves@gmail.com>
2019-12-20 21:06:20 +0800

    chore: remove socket.io

--- example/index.ts
+++ example/index.ts
@@ -1,5 +1,4 @@
 import tsKoa from "../lib/index";
-import * as wsio from "socket.io";
 const app = new tsKoa();
 
 app.use(async (ctx, next) => {
@@ -13,20 +12,4 @@ app.use(async (ctx, next) => {
 
 const server = app.createServer().listen("9000", () => {
   console.log("tsKoa serveing...");
-});
-
-const ws = wsio(server);
-
-ws.on("connection", function(socket) {
-  socket.on("broadcast", data => {
-    socket.broadcast.send(data);
-  });
-
-  socket.on("message", data => {
-    socket.send(data);
-  });
-
-  socket.on("disconnect", socket => {
-    ws.emit("user disconnected");
-  });
 });
