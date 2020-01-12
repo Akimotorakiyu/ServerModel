1a4e7e0bcc8ceafb33a00434b8f075a3b029d1fd
鸽子·湫曗 <autumnsilverleaves@gmail.com>
2020-01-12 14:51:23 +0800

    change: 添加socket.io

--- example/index.ts
+++ example/index.ts
@@ -1,6 +1,8 @@
 import tsKoa from "../lib/index";
 const app = new tsKoa();
 
+import * as IO from "socket.io";
+
 app.use(async (ctx, next) => {
   ctx.res.write("hello world! 1");
   ctx.req.on("data", data => {
@@ -10,6 +12,24 @@ app.use(async (ctx, next) => {
   next();
 });
 
-const server = app.createServer().listen("9000", () => {
+const server = app.createServer();
+
+const io = IO(server);
+
+server.listen("9000", () => {
   console.log("tsKoa serveing...");
+});
+
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("connect", function() {
+    console.log("user disconnected");
+    socket.emit("message", "welcome!");
+  });
+  socket.on("message", data => {
+    socket.broadcast.send(data);
+  });
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
 });
