4e9d9d6d31549b306dfcbe91d1b1f2ea41795af5
瓦姆乌·湫曗 <autumnsilverleaves@gmail.com>
2020-03-20 22:33:07 +0800

    new onionRings

--- lib/onionRings.ts
+++ lib/onionRings.ts
@@ -1,18 +1,23 @@
 type Middleware<T> = (ctx: T, next: () => Promise<void>) => Promise<void>;
 
-export default function onionRings<T>(middleware: Middleware<T>[]) {
-  async function theNext(ref: { ctx: T; index: number }) {
-    const fn = middleware[ref.index++];
-    if (fn)
-      await fn(ref.ctx, () => {
-        return theNext(ref);
-      });
-  }
+async function theNext<T>(ref: {
+  ctx: T;
+  index: number;
+  middleware: Middleware<T>[];
+}) {
+  const fn = ref.middleware[ref.index++];
+  if (fn)
+    await fn(ref.ctx, () => {
+      return theNext(ref);
+    });
+}
 
+export default function onionRings<T>(middleware: Middleware<T>[]) {
   return (ctx: T) => {
     return theNext({
       ctx,
-      index: 0
+      index: 0,
+      middleware
     });
   };
 }
